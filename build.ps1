868de229f631cbe28729e1a5c49d64e15421401f
Markus Maal <mina@markusmaal.ee>
2026-06-20 20:41:50 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -7,9 +7,16 @@ Write-Host -ForegroundColor Green -NoNewLine "$b   "
 Write-Host "markuse arvuti asjad"
 Write-Host -ForegroundColor Blue "	 $b "
 Write-Output ""
-
-dotnet publish MasCpanel -c Release -o out -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugSymbols=false
-dotnet publish MasFlashDrv -c Release -o out -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugSymbols=false
+$build_flags = ("-c", "Release",
+				"-o", "out",
+				"-p:PublishReadyToRun=true",
+				"-p:PublishSingleFile=true",
+				"-p:PublishTrimmed=true",
+				"--self-contained", "true",
+				"-p:IncludeNativeLibrariesForSelfExtract=true",
+				"-p:DebugSymbols=false")
+dotnet publish MasCpanel $build_flags
+dotnet publish MasFlashDrv $build_flags
 $mas_root = [environment]::getfolderpath(“userprofile”) + "/.mas"
 $platform = [System.Environment]::OSVersion.Platform
 $suff = ""
